923c14cd803d80c662d6f9eca299e161af5de731
ARUNAediri96 <arunaedirisinghe96@gmail.com>
2022-07-08 08:19:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/androidalarmclockapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidalarmclockapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.javapapers.androidalarmclock.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/androidalarmclockapplication/activity_my.java
@@ -0,0 +1,16 @@
+package com.example.androidalarmclockapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.javapapers.androidalarmclock.R;
+
+public class activity_my extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+}
